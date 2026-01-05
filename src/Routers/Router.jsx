d2b3c906fbdaf56922808592dce2023fd185ef47
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-05 20:27:31 +0600

    router add

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
