1988b49bc0b62c35d6a072255602280779c83cb1
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-05 20:43:14 +0600

    color setup

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
 
 const Root = () => {
   return (
     <div>
       <Outlet />
+      <Footer />
     </div>
   );
 };
