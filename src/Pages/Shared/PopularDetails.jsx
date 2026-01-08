8251b2b294772f238d0dc7ea644fe2a4094c5d31
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-08 23:43:25 +0600

    data card use

--- src/Pages/Shared/PopularDetails.jsx
+++ src/Pages/Shared/PopularDetails.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const PopularDetails = ({ popularItem }) => {
   const { name, image, recipe, price } = popularItem;
   return (
-    <div className="flex justify-between items-center gap-4 bg-nu20 px-4 py-2 h-full rounded-md">
-      <img className="w-36 h-26" src={image} alt="" />
+    <div className="flex justify-between items-center gap-6 bg-nu20 px-4 py-2 h-full rounded-md">
+      <img
+        className="w-34 h-26  rounded-tr-[200px] rounded-bl-[200px] rounded-br-[200px]"
+        src={image}
+        alt=""
+      />
       <div className="">
         <p className="font-cinzel uppercase mb-2">{name} ----------------</p>
         <p className="text-nu40">{recipe}</p>
