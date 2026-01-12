ed97d6cf906f5587277cf4b4818f2543069dde45
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-13 00:35:10 +0600

    shop bred crumb add

--- src/Pages/OurShop/OurShop.jsx
+++ src/Pages/OurShop/OurShop.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import BredCrumb from "../../Components/BredCrumb/BredCrumb";
+import { Helmet } from "react-helmet";
+import OurShopImg from "../../assets/images/our-shop/our-shop1.png";
 
 const OurShop = () => {
   return (
     <div>
-      <BredCrumb />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Food Corner - Our Shop</title>
+      </Helmet>
+      <div className="">
+        <BredCrumb
+          title={"OUR SHOP"}
+          subTitle={"Would you like to try a dish?"}
+          BredCrumbImg={OurShopImg}
+        />
+      </div>
     </div>
   );
 };
