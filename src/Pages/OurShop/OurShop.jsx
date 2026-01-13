c2f43799f4fb674296dc837fe9422db930a60203
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-13 20:43:42 +0600

    tab add

--- src/Pages/OurShop/OurShop.jsx
+++ src/Pages/OurShop/OurShop.jsx
@@ -17,6 +17,60 @@ const OurShop = () => {
           BredCrumbImg={OurShopImg}
         />
       </div>
+      <div className="lg:py-25 md:py-22 sm:py-20 py-18 bg-nu10">
+        <div className="container-2">
+          <div className="tabs tabs-border  border-0 flex justify-center items-center">
+            <input
+              type="radio"
+              name="my_tabs_2"
+              className="tab text-nu60 font-medium text-[24px] duration-500"
+              aria-label="Salad"
+            />
+            <div className="tab-content border-0 px-10 py-10">
+              Tab content 1
+            </div>
+
+            <input
+              type="radio"
+              name="my_tabs_2"
+              className="tab text-nu60 font-medium text-[24px] duration-500"
+              aria-label="Pizza"
+              defaultChecked
+            />
+            <div className="tab-content border-0  px-10 py-10">
+              Tab content 2
+            </div>
+
+            <input
+              type="radio"
+              name="my_tabs_2"
+              className="tab text-nu60 font-medium text-[24px] duration-500"
+              aria-label="Soups"
+            />
+            <div className="tab-content border-0 px-10 py-10">
+              Tab content 3
+            </div>
+            <input
+              type="radio"
+              name="my_tabs_2"
+              className="tab text-nu60 font-medium text-[24px] duration-500"
+              aria-label="Desserts"
+            />
+            <div className="tab-content border-0 px-10 py-10">
+              Tab content 4
+            </div>
+            <input
+              type="radio"
+              name="my_tabs_2"
+              className="tab text-nu60 font-medium text-[24px] duration-500 "
+              aria-label="Drinks"
+            />
+            <div className="tab-content border-0 px-10 py-10">
+              Tab content 5
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
