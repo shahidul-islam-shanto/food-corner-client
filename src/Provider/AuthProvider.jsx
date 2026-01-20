62e1a1898bb43aab87b5ca48ad27298029b3c53f
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-20 23:58:30 +0600

    current user observer add

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,23 +1,48 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // create user site
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   //   singin user email and password
   const singInEmailPassword = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  //   log out user
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //  current user observer
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (allUser) => {
+      const userEmail = allUser?.email || allUser?.email;
+      const loginUser = { email: userEmail };
+      setUser(loginUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
   const authInfo = {
     user,
+    createUser,
+    singInEmailPassword,
+    logOut,
   };
 
   return (
