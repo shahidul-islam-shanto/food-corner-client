ce8c846bac919f8ab1b607d1a5a22093d0f61331
shahidul-islam <shahidulislam47326@gmail.com>
2026-01-28 20:59:50 +0600

    add to card start

--- src/Pages/Recommends/RecommendCard.jsx
+++ src/Pages/Recommends/RecommendCard.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 const RecommendCard = ({ recommendCord }) => {
   const { image, name, recipe } = recommendCord;
+
+  const handleAddToCart = (food) => {
+    console.log(food);
+  };
+
   return (
     <div className="bg-nu20 px-6 py-6 rounded-md w-full h-full">
       <img className=" mb-6 rounded-md" src={image} alt="" />
@@ -10,7 +15,10 @@ const RecommendCard = ({ recommendCord }) => {
         <p className="text-nu60 font-normal h-full">{recipe}</p>
       </div>
       <div className="text-center">
-        <button className="px-6 py-3 border-b-2 border-nu102 hover:border-nu60 font-medium bg-nu30 hover:bg-nu60 text-nu102 duration-300 uppercase rounded-lg">
+        <button
+          onClick={() => handleAddToCart(recommendCord)}
+          className="px-6 py-3 border-b-2 border-nu102 hover:border-nu60 font-medium bg-nu30 hover:bg-nu60 text-nu102 duration-300 uppercase rounded-lg"
+        >
           add to cart
         </button>
       </div>
