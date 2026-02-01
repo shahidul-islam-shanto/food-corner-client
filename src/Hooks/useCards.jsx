d85ad8b96bda8d65ac6cbe7f94d128f2e020ff74
shahidul-islam <shahidulislam47326@gmail.com>
2026-02-01 23:37:40 +0600

    user email use

--- src/Hooks/useCards.jsx
+++ src/Hooks/useCards.jsx
@@ -1,12 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
 
 const useCards = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: card = [] } = useQuery({
-    queryKey: ["card"],
+  //   user email use
+  const { user } = useAuth();
+  const { refetch, data: card = [] } = useQuery({
+    queryKey: ["card", user?.email],
     queryFn: async () => {
-      const res = await axiosSecure.get("/cards");
+      const res = await axiosSecure.get(`/cards?email=${user.email}`);
       return res.data;
     },
   });
