6c20dec215c0c74f0534faa52d04323a2d3533eb
shahidul-islam <shahidulislam47326@gmail.com>
2026-02-03 01:23:27 +0600

    dashboard side ber start

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import Logo from "../../assets/images/hero-img/logo.png";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>this is a Dashboard</h1>
-      <div className="grid grid-cols-12 gap-6">
+    <div className="">
+      <div className="grid grid-cols-12 gap-6 ">
         {/* dashboard side ber */}
         <div className="col-span-4">
-          <div className="bg-nu40 px-10 py-10">
+          <div className="bg-nu108 px-10 py-10">
+            <div className="">
+              <div className="text-center">
+                <img className="w-18 h-18 border-2 border-nu10 bg-nu10 rounded-full " src={Logo} alt="" />
+              </div>
+              <div className="">
+                <h2 className="">Food Corner</h2>
+                <h4 className="">Restaurant</h4>
+              </div>
+            </div>
+
             <ul>
               <li>
                 <NavLink to={"/dashboard/dashCard"}>My Card</NavLink>
@@ -24,7 +34,9 @@ const Dashboard = () => {
         </div>
         {/* dashboard Main  */}
         <div className="col-span-8">
-          <Outlet />
+          <div className="py-10">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
