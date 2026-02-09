2ffa9e99138a21dd9be01038013186e23747accd
shahidul-islam <shahidulislam47326@gmail.com>
2026-02-10 00:33:24 +0600

    total price sum

--- src/Pages/Dashboard/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems.jsx
@@ -3,10 +3,14 @@ import useCards from "../../Hooks/useCards";
 
 const ManageItems = () => {
   const [cart] = useCards();
+  const totalPrice = cart.reduce((total, items) => {
+    return total + items.price;
+  }, 0);
   return (
     <div>
       <div className="">
-        <h1>Manage Items: {cart.length}</h1>
+        <h2>Manage Items: {cart.length}</h2>
+        <h2>Total Price: {totalPrice}</h2>
       </div>
     </div>
   );
