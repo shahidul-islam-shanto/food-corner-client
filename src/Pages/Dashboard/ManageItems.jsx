ba1272ad38e3bd8f997275140848a4741e339e02
shahidul-islam <shahidulislam47326@gmail.com>
2026-02-10 01:19:41 +0600

    table item

--- src/Pages/Dashboard/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import useCards from "../../Hooks/useCards";
+import { MdEditSquare } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const ManageItems = () => {
   const [cart] = useCards();
@@ -9,8 +11,56 @@ const ManageItems = () => {
   return (
     <div>
       <div className="">
-        <h2>Manage Items: {cart.length}</h2>
-        <h2>Total Price: {totalPrice}</h2>
+        <div className="flex justify-between items-center px-6 mb-12">
+          <h2>Manage Items: {cart.length}</h2>
+          <h2>Total Price: ${totalPrice}</h2>
+          <button className="px-6 py-2 bg-nu70 font-semibold rounded-lg text-nu10">
+            Pay
+          </button>
+        </div>
+        <div className="px-10">
+          <div className="">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th className="text-nu60">Item Image</th>
+                  <th className="text-nu60">Item Name</th>
+                  <th className="text-nu60">Price</th>
+                  <th className="text-nu60">Action</th>
+                  <th className="text-nu60">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((items) => (
+                  <tr>
+                    <td>
+                      <img
+                        className="w-14 h-14 rounded-full"
+                        src={items.image}
+                        alt=""
+                      />
+                    </td>
+                    <td>
+                      <h5>{items.name}</h5>
+                    </td>
+                    <td>${items.price}</td>
+                    <th>
+                      <button className="px-3 py-3 bg-nu101 text-nu10 rounded-lg">
+                        <MdEditSquare className="text-[24px]" />
+                      </button>
+                    </th>
+                    <th>
+                      <button className="px-3 py-3 bg-nu109 text-nu10 rounded-lg">
+                        <RiDeleteBin6Line className="text-[24px]" />
+                      </button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
