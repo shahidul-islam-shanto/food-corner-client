ad1cf6ac5b9e82841b083747b86592a092f8e366
shahidul-islam <shahidulislam47326@gmail.com>
2026-02-11 00:36:54 +0600

    item delete use

--- src/Pages/Dashboard/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems.jsx
@@ -2,12 +2,38 @@ import React from "react";
 import useCards from "../../Hooks/useCards";
 import { MdEditSquare } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const ManageItems = () => {
   const [cart] = useCards();
+  const secureAxios = useAxiosSecure();
   const totalPrice = cart.reduce((total, items) => {
     return total + items.price;
   }, 0);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        secureAxios.delete(`/carts/${id}`).then((res) => {
+          console.log(res.data);
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+        });
+      }
+    });
+  };
   return (
     <div>
       <div className="">
@@ -19,16 +45,16 @@ const ManageItems = () => {
           </button>
         </div>
         <div className="px-10">
-          <div className="">
+          <div className="overflow-x-auto">
             <table className="table">
               {/* head */}
               <thead>
                 <tr>
-                  <th className="text-nu60">Item Image</th>
-                  <th className="text-nu60">Item Name</th>
-                  <th className="text-nu60">Price</th>
-                  <th className="text-nu60">Action</th>
-                  <th className="text-nu60">Action</th>
+                  <th className="text-nu60 text-[20px]">Item Image</th>
+                  <th className="text-nu60 text-[20px]">Item Name</th>
+                  <th className="text-nu60 text-[20px]">Price</th>
+                  <th className="text-nu60 text-[20px]">Action</th>
+                  <th className="text-nu60 text-[20px]">Action</th>
                 </tr>
               </thead>
               <tbody>
@@ -42,7 +68,7 @@ const ManageItems = () => {
                       />
                     </td>
                     <td>
-                      <h5>{items.name}</h5>
+                      <h6>{items.name}</h6>
                     </td>
                     <td>${items.price}</td>
                     <th>
@@ -51,7 +77,10 @@ const ManageItems = () => {
                       </button>
                     </th>
                     <th>
-                      <button className="px-3 py-3 bg-nu109 text-nu10 rounded-lg">
+                      <button
+                        onClick={() => handleDelete(items._id)}
+                        className="px-3 py-3 bg-nu109 text-nu10 rounded-lg"
+                      >
                         <RiDeleteBin6Line className="text-[24px]" />
                       </button>
                     </th>
