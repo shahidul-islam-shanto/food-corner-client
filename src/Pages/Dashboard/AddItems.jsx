f4824bcb4aa5b6e108fbeee2c8a145e61b0b24b4
shahidul-islam <shahidulislam47326@gmail.com>
2026-03-04 21:59:18 +0600

    react select add

--- src/Pages/Dashboard/AddItems.jsx
+++ src/Pages/Dashboard/AddItems.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import Select from "react-select";
 
 const AddItems = () => {
+  const [selectedOption1, setSelectedOption1] = useState(null);
+  const [selectedOption2, setSelectedOption2] = useState(null);
+
+  const options = [
+    { value: "apple", label: "Apple" },
+    { value: "banana", label: "Banana" },
+    { value: "mango", label: "Mango" },
+  ];
   return (
     <>
       <div className="">
@@ -35,23 +44,27 @@ const AddItems = () => {
                           <label htmlFor="name" className="font-semibold">
                             Recipe name*
                           </label>
-                          <input
-                            type="email"
-                            name="email"
-                            placeholder="Recipe name"
-                            className="w-full px-3 py-3 mb-4 mt-2  placeholder:text-nu60 rounded-xl bg-nu10   outline-none"
-                          />
+                          <div className="w-88">
+                            <Select
+                              options={options}
+                              value={selectedOption1}
+                              onChange={setSelectedOption1}
+                              placeholder="Select a fruit"
+                            />
+                          </div>
                         </div>
-                         <div className="">
+                        <div className="">
                           <label htmlFor="name" className="font-semibold">
                             Recipe name*
                           </label>
-                          <input
-                            type="email"
-                            name="email"
-                            placeholder="Recipe name"
-                            className="w-full px-3 py-3 mb-4 mt-2  placeholder:text-nu60 rounded-xl bg-nu10   outline-none"
-                          />
+                          <div className="w-88" >
+                            <Select
+                              options={options}
+                              value={selectedOption2}
+                              onChange={setSelectedOption2}
+                              placeholder="Select a fruit"
+                            />
+                          </div>
                         </div>
                       </div>
                     </form>
